c380947bea916ab58b06048d0a3ce6b2d729ad98
Kuba Orlik <kontakt@jakub-orlik.pl>
2016-08-16 11:09:23 +0200

    Update graphviz script to Pandoc 1.16

--- graphviz.py
+++ graphviz.py
@@ -1,27 +1,30 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
 """
 Pandoc filter to process code blocks with class "graphviz" into
 graphviz-generated images.
 
-Needs pygraphviz
+Needs pygraphviz and Pandoc 1.16
 """
 
 import os
 import sys
+import hashlib
 
 import pygraphviz
 
-from pandocfilters import toJSONFilter, Para, Image, get_filename4code, get_caption, get_extension
+from pandocfilters import toJSONFilter, Para, Image
 
 
 def graphviz(key, value, format, _):
     if key == 'CodeBlock':
         [[ident, classes, keyvals], code] = value
         if "graphviz" in classes:
-            caption, typef, keyvals = get_caption(keyvals)
-            filetype = get_extension(format, "png", html="png", latex="pdf")
-            dest = get_filename4code("graphviz", code, filetype)
+            typef = "png"
+            caption = ""
+            keyvals = []
+            filetype = "png"
+            dest = "graphviz-images/" + hashlib.md5(code.encode()).hexdigest() + "." + filetype
 
             if not os.path.isfile(dest):
                 g = pygraphviz.AGraph(string=code)
@@ -29,7 +32,7 @@ def graphviz(key, value, format, _):
                 g.draw(dest)
                 sys.stderr.write('Created image ' + dest + '\n')
 
-            return Para([Image([ident, [], keyvals], caption, ['./' + dest, typef])])
+            return [{"t":"Para","c":[{"t":"Image","c":[["",[],[]],[],[dest,"fig:"]]}]}]
 
 if __name__ == "__main__":
     toJSONFilter(graphviz)
